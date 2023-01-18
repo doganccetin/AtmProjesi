e6938b2ec7d10c255db40b18cdd8376fe178e413
Doğan Işıklı <dogancetin2125@gmail.com>
2023-01-18 16:18:30 +0300

    ATM Projesi
    ATM Projesi Switch-Case ile.

---
+++ AtmProjesi/src/CetinBank.java
@@ -0,0 +1,74 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class CetinBank {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String kullaniciAdi, sifre;
+        int kalanHak = 3;
+        long bakiye = 100000000;
+        int secenek;
+
+        while (kalanHak > 0) {
+            System.out.print("Kullanıcı Adı veya Hesap Numaranız: ");
+            kullaniciAdi = input.nextLine();
+            System.out.print("Şifreniz: ");
+            sifre = input.nextLine();
+
+            if (kullaniciAdi.equals("DenemeHesabı") && sifre.equals("123asd123"))
+            {
+                System.out.println("Merhaba Çetin Bank'a Hoşgeldiniz.");
+                do
+                {
+                    System.out.println("Lütfen Yapmak İstediğiniz İşlemi Seçiniz...");
+                    System.out.println("1-Para Çekme\n" +
+                            "2-Para Yatırma\n" +
+                            "3-Bakiye Sorgula\n" +
+                            "4-Çıkış Yap");
+                    secenek = input.nextInt();
+
+                    switch (secenek)
+                    {
+                        case 1:
+                            System.out.println("Para Miktarı: ");
+                            int miktar = input.nextInt();
+                            bakiye += miktar;
+                            break;
+                        case 2:
+                            System.out.println("Para Miktarı: ");
+                            miktar = input.nextInt();
+                            if (miktar > bakiye)
+                            {
+                                System.out.println("Bakiye Yetersiz!!!");
+                            }
+                            else
+                            {
+                                bakiye -= miktar;
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Güncel Bakiye: " + bakiye);
+                            break;
+                        case 4:
+                            System.out.println("Tekrar Görüşmek Üzere...");
+                            break;
+                        default:
+                            System.out.println("Geçersiz işlem seçimi.");
+                    }
+                }
+                while (secenek != 4);
+            }
+            else
+            {
+                kalanHak--;
+                System.out.println("Hatalı Kullanıcı Adı veya Şifre!!!");
+                System.out.println("Kalan Hakkınız: " + kalanHak);
+            }
+
+            if (kalanHak == 0) {
+                System.out.println("Hesabınız Bloke Olmuştur!!!");
+                break;
+            }
+        }
+    }
+}
