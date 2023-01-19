973f36e3141a79696c58681ebba42baa5598653c
ask0 <voidask0@gmail.com>
2023-01-19 18:38:24 +0300

    Update CetinBank.java
    Fixed overloop after quitting from the account and naming mistakes

--- AtmProjesi/src/CetinBank.java
+++ AtmProjesi/src/CetinBank.java
@@ -2,31 +2,28 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 public class CetinBank {
     public static void main(String[] args) {
-
+        
         Scanner input = new Scanner(System.in);
         String kullaniciAdi, sifre;
         int kalanHak = 3;
         long bakiye = 100000000;
-        int secenek;
-
+        int secenek = 0;
         while (kalanHak > 0) {
             System.out.print("Kullanıcı Adı veya Hesap Numaranız: ");
             kullaniciAdi = input.nextLine();
             System.out.print("Şifreniz: ");
             sifre = input.nextLine();
-
             if (kullaniciAdi.equals("DenemeHesabı") && sifre.equals("123asd123"))
             {
                 System.out.println("Merhaba Çetin Bank'a Hoşgeldiniz.");
-                do
+                while(secenek!=4)
                 {
                     System.out.println("Lütfen Yapmak İstediğiniz İşlemi Seçiniz...");
-                    System.out.println("1-Para Çekme\n" +
-                            "2-Para Yatırma\n" +
+                    System.out.println("1-Para Yatırma\n" +
+                            "2-Para Çekme\n" +
                             "3-Bakiye Sorgula\n" +
                             "4-Çıkış Yap");
                     secenek = input.nextInt();
-
                     switch (secenek)
                     {
                         case 1:
@@ -51,24 +48,26 @@ public class CetinBank {
                             break;
                         case 4:
                             System.out.println("Tekrar Görüşmek Üzere...");
+                            kalanHak = 0;
                             break;
                         default:
                             System.out.println("Geçersiz işlem seçimi.");
                     }
                 }
-                while (secenek != 4);
             }
             else
             {
                 kalanHak--;
-                System.out.println("Hatalı Kullanıcı Adı veya Şifre!!!");
-                System.out.println("Kalan Hakkınız: " + kalanHak);
+                if (kalanHak == 0) {
+                    System.out.println("Hesabınız Bloke Olmuştur!!!");
+                    break;
+                }
+                else{
+                    System.out.println("Hatalı Kullanıcı Adı veya Şifre!!!");
+                    System.out.println("Kalan Hakkınız: " + kalanHak);
+                }
             }
 
-            if (kalanHak == 0) {
-                System.out.println("Hesabınız Bloke Olmuştur!!!");
-                break;
-            }
         }
     }
 }
